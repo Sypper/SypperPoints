66fcc72161644086818c152dc8c473cbcefddb38
Sypper <jvictor.mg.jv@gmail.com>
2020-05-10 20:54:28 -0300

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ var options =
 		username: "sypperjnb",
 		password: "oauth:z6cz5w2orcs4mprobghk4pv719wu3i",  
 	},
-	channels: ['#gaules', '#inr1tv', '#marcobc', '#murilo_rt', '#jeffaocs', '#pescocofino', '#csrfps', '#mibrtv', '#marcaorx', '#sheviiioficial', '#brnwowzk1', '#gabepeixe', '#stev3z', '#Vovo', '#furiatv', '#narigafps', '#stereonline', '#skipnho', '#skipnholive', '#saullo', '#mch_agg', '#eu_sou_um_brinquedo', '#nemshow97', '#recruta_xnavyzzz', ] //ADICIONA OS CANAIS AQ COM '#NOME'
+	channels: ['#gaules', '#inr1tv', '#marcobc', '#murilo_rt', '#jeffaocs', '#pescocofino', '#csrfps', '#mibrtv', '#marcaorx', '#sheviiioficial', '#brnwowzk1', '#gabepeixe', '#stev3z', '#Vovo', '#furiatv', '#narigafps', '#stereonline', '#skipnho', '#skipnholive', '#saullo', '#mch_agg', '#eu_sou_um_brinquedo', '#nemshow97', '#recruta_xnavyzzz', '#cloudpixelll', ] //ADICIONA OS CANAIS AQ COM '#NOME'
 };
 
 var client = new tmi.client(options);
