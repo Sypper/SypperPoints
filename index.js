7c6db392188a405f4ee0275e1008b42379684aa4
Sypper <jvictor.mg.jv@gmail.com>
2021-01-10 20:39:22 -0300

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ var options =
 		username: "sypperjnb",
 		password: "oauth:z6cz5w2orcs4mprobghk4pv719wu3i",  
 	},
-	channels: ['#gaules', '#inr1tv', '#marcobc', '#murilo_rt', '#jeffaocs', '#pescocofino', '#csrfps', '#mibrtv', '#marcaorx', '#sheviiioficial', '#brnwowzk1', '#gabepeixe', '#stev3z', '#Vovo', '#furiatv', '#narigafps', '#stereonline', '#skipnho', '#skipnholive', '#saullo', '#mch_agg', '#Umbrinquedo', '#nemshow97', '#recruta_xnavyzzz', '#cloudpixelll', '#vintagefps1', '#sterinRR', '#mandyzz', '#dilera', '#nobru',  ] //ADICIONA OS CANAIS AQ COM '#NOME'
+	channels: ['#gaules', '#inr1tv', '#marcobc', '#murilo_rt', '#jeffaocs', '#pescocofino', '#csrfps', '#mibrtv', '#marcaorx', '#sheviiioficial', '#brnwowzk1', '#gabepeixe', '#stev3z', '#Vovo', '#furiatv', '#narigafps', '#stereonline', '#skipnho', '#skipnholive', '#saullo', '#mch_agg', '#Umbrinquedo', '#nemshow97', '#recruta_xnavyzzz', '#cloudpixelll', '#vintagefps1', '#stereofflinex', '#mandyzz', '#dilera', '#nobru',  ] //ADICIONA OS CANAIS AQ COM '#NOME'
 };
 
 var client = new tmi.client(options);
